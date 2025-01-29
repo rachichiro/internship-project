fcf39ba1b7eabcd2ac3363291298c988947d90e1
Rachael <rachaelmelfi@gmail.com>
2025-01-28 22:12:37 -0600

    Allure Logging

--- features/environment.py
+++ features/environment.py
@@ -13,9 +13,9 @@ def browser_init(context, scenario_name):
     :param context: Behave context
     """
     ##Chrome Webdriver##
-    # driver_path = ChromeDriverManager().install()
-    # service = Service(driver_path)
-    # context.driver = webdriver.Chrome(service=service)
+    driver_path = ChromeDriverManager().install()
+    service = Service(driver_path)
+    context.driver = webdriver.Chrome(service=service)
 
     ###Firefox###
 
@@ -43,21 +43,21 @@ def browser_init(context, scenario_name):
 
     ### BROWSERSTACK ###
 
-    bs_user = 'rachaelmelfi_XMgZ93'
-    bs_key = 'JpaG7rkXtGSMFAPqqTCs'
-    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
-
-    options = Options()
-    bstack_options = {
-        "os": "Windows",
-        "osVersion": "11",
-        'browserName': 'chrome',
-        'sessionName': scenario_name,
-    }
-
-
-    options.set_capability('bstack:options', bstack_options)
-    context.driver = webdriver.Remote(command_executor=url, options=options)
+    # bs_user = 'rachaelmelfi_XMgZ93'
+    # bs_key = 'JpaG7rkXtGSMFAPqqTCs'
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    # options = Options()
+    # bstack_options = {
+    #     "os": "Windows",
+    #     "osVersion": "11",
+    #     'browserName': 'chrome',
+    #     'sessionName': scenario_name,
+    # }
+    #
+    #
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(5)
